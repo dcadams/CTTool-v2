8dc445c1c7490a11130661e2e0bcfff21af68397
Arpit Gandhi <arpit.gandhi@xoriant.com>
2022-04-19 16:50:42 +0530

    Add file Input Output ajax call

--- app_cttoolv2/urls.py
+++ app_cttoolv2/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, convert_course
 
 urlpatterns = [
     path('', index, name='index'),
+    path('convert_course/', convert_course, name='index'),
 ]
--- app_cttoolv2/views.py
+++ app_cttoolv2/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.http import HttpResponse
+import tarfile
 
 
 def index(request):
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {}
     return render(request, 'index.html', context)
+
+def convert_course(request):
+    context = {}
+    tar_file = request.FILES.get('input-file')
+
+    path = default_storage.save(tar_file.name, ContentFile(tar_file.read()))
+
+    tar = tarfile.open(path, "r:gz")
+    my_file = tar.fileobj
+
+    response = HttpResponse(my_file, content_type=tar_file.content_type)
+    return response
+
